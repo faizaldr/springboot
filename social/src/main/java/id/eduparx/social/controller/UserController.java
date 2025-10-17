2a77ae4c8d55917544227c5647c3bb972e1cbeb7
faizaldr <fdr00zenc@gmail.com>
2025-10-17 19:28:42 +0700

    ok

--- social/src/main/java/id/eduparx/social/controller/UserController.java
+++ social/src/main/java/id/eduparx/social/controller/UserController.java
@@ -5,19 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 // import org.springframework.security.crypto.password.PasswordEncoder;
-
-import id.eduparx.social.dto.UserDto;
-import id.eduparx.social.repository.UserRepository;
-import id.eduparx.social.service.UserService;
-import id.eduparx.social.service.UserServiceImpl;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import id.eduparx.social.repository.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import id.eduparx.social.dto.UserDto;
+import id.eduparx.social.model.User;
+import id.eduparx.social.service.UserServiceImpl;
+
+// PASSWORD
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,7 +25,15 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<UserDto>> getMethodName() {
-        List<UserDto> users= userService.getAll();
+        List<UserDto> users = userService.getAll();
         return ResponseEntity.ok(users);
     }
+
+    @PostMapping("/")
+    public ResponseEntity<UserDto> createUser(@RequestBody String username, @RequestBody String email,
+            @RequestBody String password, @RequestBody User.Role role) {
+        UserDto userDto = userService.createUser(username, email, password, role);
+        return ResponseEntity.ok(userDto);
+    }
+
 }
